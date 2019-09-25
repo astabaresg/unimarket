150b070554ee297b496cfcf10d467e80ae71e960
Jhon Ospina <jhonjames0510@gmail.com>
2019-09-25 12:59:59 -0500

    creación clase de pruebas james

---
+++ unimarket/pruebas/src/main/java/com/taheos/unimarket/pruebas/TestJames.java
@@ -0,0 +1,47 @@
+package com.taheos.unimarket.pruebas;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.taheos.unimarket.entidades.Persona;
+
+/**
+ * Clase de pruebas dedicada para la pruebas de las entidades
+ * 
+ * @version 1.0
+ */
+@RunWith(Arquillian.class)
+public class TestJames {
+	/**
+	 * instancia para realizar las transaciones con las entidades
+	 * 
+	 */
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	/**
+	 * genera el archivo del despliegue de pruebas
+	 * 
+	 * @return genera un archivo de configuracion web
+	 */
+	@Deployment
+	public static Archive<?> createTestArchive() {
+		return ShrinkWrap.create(WebArchive.class, "prueba.war").addPackage(Persona.class.getPackage())
+				.addAsResource("persistenceForTest.xml", "META-INF/persistence.xml")
+				.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+
+	}
+	@Test
+	public void generarTest() {
+		
+	}
+}
