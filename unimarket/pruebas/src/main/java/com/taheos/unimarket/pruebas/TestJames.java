aa52c9b80802f754325367d56cac5dd7085baa70
Jhon Ospina <jhonjames0510@gmail.com>
2019-09-25 17:54:36 -0500

    PRUEBA CRUD COMPRA, DETALLECOPRA

--- unimarket/pruebas/src/main/java/com/taheos/unimarket/pruebas/TestJames.java
+++ unimarket/pruebas/src/main/java/com/taheos/unimarket/pruebas/TestJames.java
@@ -22,10 +22,15 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import com.ctc.wstx.evt.WNotationDeclaration;
+import com.taheos.unimarket.entidades.Compra;
+import com.taheos.unimarket.entidades.DetalleCompra;
 import com.taheos.unimarket.entidades.Persona;
 import com.taheos.unimarket.entidades.Producto;
+import com.taheos.unimarket.entidades.Usuario;
 import com.taheos.unimarket.enums.Categoria;
 import com.taheos.unimarket.enums.Disponibilidad;
+import com.taheos.unimarket.enums.MetodoPago;
 
 /**
  * Clase de pruebas dedicada para la pruebas de las entidades
@@ -54,9 +59,12 @@ public class TestJames {
 
 	}
 
+	/**
+	 * Permite probar: agregar un producto
+	 */
 	@Test
 	@Transactional(value = TransactionMode.ROLLBACK)
-	@UsingDataSet({ "producto.json" })
+	@UsingDataSet({ "Producto.json" })
 	public void agregarProductoTest() {
 
 		Producto nuevoP = new Producto();
@@ -77,6 +85,9 @@ public class TestJames {
 
 	}
 
+	/**
+	 * Permite probar: buscar un producto
+	 */
 	@Test
 	@Transactional(value = TransactionMode.ROLLBACK)
 	@UsingDataSet({ "Producto.json" })
@@ -85,9 +96,12 @@ public class TestJames {
 		Assert.assertEquals("Pc ASUS", producto.getNombre());
 	}
 
+	/**
+	 * Permite probar: eliminar un producto
+	 */
 	@Test
 	@Transactional(value = TransactionMode.ROLLBACK)
-	@UsingDataSet({ "producto.json" })
+	@UsingDataSet({ "Producto.json" })
 	public void eliminarProductoTest() {
 		entityManager.remove(entityManager.find(Producto.class, 4321L));
 
@@ -96,9 +110,12 @@ public class TestJames {
 
 	}
 
+	/**
+	 * Permite probar: modificar un producto
+	 */
 	@Test
 	@Transactional(value = TransactionMode.ROLLBACK)
-	@UsingDataSet({ "producto.json" })
+	@UsingDataSet({ "Producto.json" })
 	public void modificarProductoTest() {
 
 		Producto cambio = entityManager.find(Producto.class, 6789L);
@@ -106,6 +123,136 @@ public class TestJames {
 		entityManager.merge(cambio);
 		Producto nuevo = entityManager.find(Producto.class, 6789L);
 		Assert.assertEquals(Disponibilidad.NO_DISPONIBLE, nuevo.getDisponibilidad());
+	}
+
+	/**
+	 * Permite probar: agregar una compra
+	 */
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "Compra.json", "Producto.json", "Persona.json" })
+	public void agregarCompraTest() {
+
+		
+		Usuario usuario =  entityManager.find(Usuario.class, "02");
+		
+		Assert.assertNotNull(usuario);
+		
+		Compra nuevaC = new Compra();
+		nuevaC.setId_compra(new Long(12345789));
+		nuevaC.setMetodoPago(MetodoPago.CREDITO);
+		nuevaC.setTotal_compra(60000.0);
+		nuevaC.setUsuario(usuario);
+		entityManager.persist(nuevaC);
+
+		Compra registrada = entityManager.find(Compra.class, 12345789L);
+		Assert.assertEquals(nuevaC, registrada);
+
+	}
+
+	/**
+	 * Permite probar: buscar una compra
+	 */
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "Compra.json", "Producto.json", "Persona.json" })
+	public void buscarCompraTest() {
+		Compra compra = entityManager.find(Compra.class, 4321L);
+		Assert.assertEquals(MetodoPago.CREDITO, compra.getMetodoPago());
+	}
+
+	/**
+	 * Permite probar: eliminar una compra
+	 */
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "Compra.json", "Producto.json", "Persona.json" })
+	public void eliminarCompraTest() {
+		entityManager.remove(entityManager.find(Compra.class, 12345L));
+
+		Compra nueva = entityManager.find(Compra.class, 12345L);
+		Assert.assertNull(nueva);
+
+	}
+
+	/**
+	 * Permite probar: modificar una compra
+	 */
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "Compra.json", "Producto.json", "Persona.json" })
+	public void modificarCompraTest() {
+
+		Compra cambio = entityManager.find(Compra.class, 4321L);
+		cambio.setMetodoPago(MetodoPago.EFECTIVO);
+		entityManager.merge(cambio);
+		Compra nuevo = entityManager.find(Compra.class, 4321L);
+		Assert.assertEquals(MetodoPago.EFECTIVO, nuevo.getMetodoPago());
+	}
+	
+	
+	/**
+	 * Permite probar: agregar un detalle compra
+	 */
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "DetalleCompra.json", "Compra.json", "Producto.json", "Persona.json" })
+	public void agregarDetalleCompraTest() {
+
+		Compra compra = entityManager.find(Compra.class, 1324L);
+		Producto producto = entityManager.find(Producto.class, 1234L);
+		
+		DetalleCompra detalleC = new DetalleCompra();
+		detalleC.setId_detalleCompra(new Long(918273));
+		detalleC.setCantidad(3);
+		detalleC.setPrecioCompra(12000.0);
+		detalleC.setCompra(compra);
+		detalleC.setProducto(producto);
+		entityManager.persist(detalleC);
+
+		DetalleCompra registrada = entityManager.find(DetalleCompra.class, 918273L);
+		Assert.assertEquals(detalleC, registrada);
+
+	}
+
+	/**
+	 * Permite probar: buscar un detalle compra
+	 */
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "DetalleCompra.json", "Compra.json", "Producto.json", "Persona.json" })
+	public void buscarDetalleCompraTest() {
+		DetalleCompra dCompra = entityManager.find(DetalleCompra.class, 9876L);
+		Assert.assertEquals(7, dCompra.getCantidad());
+	}
+
+	/**
+	 * Permite probar: eliminar un detalle compra
+	 */
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "DetalleCompra.json", "Compra.json", "Producto.json", "Persona.json" })
+	public void eliminarDetalleCompraTest() {
+		entityManager.remove(entityManager.find(DetalleCompra.class, 5678L));
+
+		DetalleCompra nueva = entityManager.find(DetalleCompra.class, 5678L);
+		Assert.assertNull(nueva);
+
+	}
+
+	/**
+	 * Permite probar: modificar un detalle compra
+	 */
+	@Test
+	@Transactional(value = TransactionMode.ROLLBACK)
+	@UsingDataSet({ "DetalleCompra.json", "Compra.json", "Producto.json", "Persona.json" })
+	public void modificarDetalleCompraTest() {
+
+		DetalleCompra cambio = entityManager.find(DetalleCompra.class, 9786L);
+		cambio.setCantidad(10);;
+		entityManager.merge(cambio);
+		DetalleCompra nuevo = entityManager.find(DetalleCompra.class, 9786L);
+		Assert.assertEquals(10, nuevo.getCantidad());
 	}
 
 }
