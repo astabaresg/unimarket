5420ac8c89e75ca1b9806bcdae4e5dd2a348e002
alvaro sebastian tabares gaviria <astg0616@gmail.com>
2019-09-25 20:46:32 -0500

    ULTIMOS COMENTARIOS QUE FALTABAN, final final no da mas

--- unimarket/persistencia/src/main/java/com/taheos/unimarket/enums/Disponibilidad.java
+++ unimarket/persistencia/src/main/java/com/taheos/unimarket/enums/Disponibilidad.java
@@ -1,5 +1,10 @@
 package com.taheos.unimarket.enums;
 
+/**
+ * Enumeracion creada con el fin de mostrar si un producto esta disponible o no
+ * @author astar
+ *
+ */
 public enum Disponibilidad {
 
 	DISPONIBLE,
--- unimarket/persistencia/src/main/java/com/taheos/unimarket/enums/MetodoPago.java
+++ unimarket/persistencia/src/main/java/com/taheos/unimarket/enums/MetodoPago.java
@@ -1,5 +1,11 @@
 package com.taheos.unimarket.enums;
 
+
+/**
+ * Enumeracion creada para determinar el metodo de pago de una compra
+ * @author astar
+ *
+ */
 public enum MetodoPago {
 
 	EFECTIVO,
