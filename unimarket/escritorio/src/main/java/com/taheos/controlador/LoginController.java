7dbf072d84d92feabff4bf16b8b3ec7ce1ec236c
alvaro sebastian tabares gaviria <astg0616@gmail.com>
2019-10-17 10:57:26 -0500

    james

---
+++ unimarket/escritorio/src/main/java/com/taheos/controlador/LoginController.java
@@ -0,0 +1,26 @@
+package com.taheos.controlador;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private JFXTextField textUser;
+
+    @FXML
+    private JFXPasswordField textPassword;
+
+    @FXML
+    private JFXButton buttonLogin;
+
+    @FXML
+    void onLoginButton(ActionEvent event) {
+
+    }
+
+}
