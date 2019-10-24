e336c180f95991b24be46889c9a44a6a9ff30d37
Jhon Ospina <jhonjames0510@gmail.com>
2019-10-23 22:05:17 -0500

    Interfaz admin y controllerAdmin creados

---
+++ unimarket/escritorio/src/main/java/com/taheos/controlador/AdminController.java
@@ -0,0 +1,73 @@
+package com.taheos.controlador;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminController {
+
+    @FXML
+    private JFXButton btnGestionarUsuario;
+
+    @FXML
+    private JFXButton btnListaProductos;
+
+    @FXML
+    private AnchorPane panelGestionarUsuario;
+
+    @FXML
+    private Label lblUsuario;
+
+    @FXML
+    private Label lblDireccion;
+
+    @FXML
+    private Label lblCorreo;
+
+    @FXML
+    private Label lblIdentificacion;
+
+    @FXML
+    private Label lblTelefono;
+
+    @FXML
+    private JFXButton btnAgregarUsuario;
+
+    @FXML
+    private JFXButton btnModificarUsuario;
+
+    @FXML
+    private JFXButton btnEliminarUsuario;
+
+    @FXML
+    private AnchorPane panelListaProductos;
+
+    @FXML
+    void onAgregarUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onEliminarUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGestionarUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onListaProductos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onModificarUsuario(ActionEvent event) {
+
+    }
+
+}
